9490d1905b3df30dd5f9ec7caaa04d49886a8dbf
Andrei Serban <andyspeed2003@gmail.com>
2024-11-02 16:26:29 +0200

    it works! ! fkhlslfjfskwegur

--- optimize/src/optimizer/main.py
+++ optimize/src/optimizer/main.py
@@ -1,7 +1,7 @@
 from src.optimizer.supply_chain_optimizer import SupplyChainOptimizer
 
 # Constants
-API_KEY = "your-api-key"
+API_KEY = "7bcd6334-bc2e-4cbf-b9d4-61cb9e868869"
 BASE_URL = "http://localhost:8080/api/v1"
 TOTAL_DAYS = 42
 
